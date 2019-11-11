feb09f13fd1e7d0bc06c16c522e37fee56a883fb
simplydv <simplydv@protonmail.com>
2019-11-10 17:41:11 -0800

    Add titles

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'November 10, 2019',
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 10, 2019',
+    }
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
